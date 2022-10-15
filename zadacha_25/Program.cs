39275fe829fbd6da4051d1ed554c5debcdb4d325
Sergey <serega110193@mail.ru>
2022-10-15 18:24:03 +0900

    добавление 25 задачи

---
+++ zadacha_25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: опишите метод, который принимает на вход два числа (A и B) 
+// и возводит число A в целую степень B.
+
+void Metod(int a, int b)
+{
+    int sum = a;
+    for (int i = 1; i < b; i++)
+    {
+        sum = sum * a;
+    }
+    Console.WriteLine(a + " в степени " + b + " равно = " + sum);
+}
+Console.WriteLine("введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите степень");
+int b = Convert.ToInt32(Console.ReadLine());
+Metod(a, b);
---
+++ zadacha_27/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+void ValueNumber()
+{
+    Console.Write("Введите любое число от 1: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+        Console.WriteLine(" ");
+    }
+}
+ValueNumber();
